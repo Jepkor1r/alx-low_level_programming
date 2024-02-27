153d6cf8d88588e9a114228330df8f016f9b2d2f
Jepkor1r <jlagat007@gmail.com>
2024-02-26 18:35:54 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+  * main - prints Programming is like building a multilingual puzzle
+  * puts - displays the output followed by a new line
+  *
+  * Return: 0
+  */
+
+int main(void)
+{	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}

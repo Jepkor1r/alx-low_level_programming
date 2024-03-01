03ced7ea7c43c8b04e8b3a8a18e288ac176aacc7
Jepkor1r <jlagat007@gmail.com>
2024-03-01 03:34:43 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,12 +12,15 @@ int main(void)
 
 	for (num1 = 0; num1 < 99; num1++)
 	{
+		putchar((num1 / 10) + '0');
+		putchar((num1 % 10) + '0');
+		putchar(' ');
 		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num1 / 10) + '0');
+			//putchar((num1 / 10) + '0');
+			//putchar((num1 % 10) + '0');
+			//putchar(' ');
+			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 			if (num1 == 98 && num2 == 99)
 			{

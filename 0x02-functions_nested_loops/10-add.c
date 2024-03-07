f12efed1739756a63a70f3890cecb2e71672a09b
Jepkor1r <jlagat007@gmail.com>
2024-03-07 04:29:17 -0800

    functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/**
+  * add -  adds two integers and returns the result
+  * @i: value 1 to be added
+  * @j: value 2 to be added
+  *
+  * Return: 0
+  */
+
+int add(int i, int j)
+{
+	int add;
+
+	add = i + j;
+	_putchar(add);
+	return (0);
+}

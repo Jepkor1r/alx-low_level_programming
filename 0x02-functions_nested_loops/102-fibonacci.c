86da736a642f8b4b46e54b33038e25aa34702fb0
Jepkor1r <jlagat007@gmail.com>
2024-03-08 01:16:34 -0800

    functions and nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+		int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
+}

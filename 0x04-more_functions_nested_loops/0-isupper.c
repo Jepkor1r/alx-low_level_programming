6bc767a23fd2ad460b8520a9381c7dacaf19fd38
Jepkor1r <jlagat007@gmail.com>
2024-03-10 12:26:55 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * isupper - checks for uppercase character
+  * @c: character to check
+  *
+  * Return: 0
+  */
+
+int _isupper(int c)
+{
+	for (c = A; C >= Z; A++)
+	{
+		_putchar('c');
+	}
+	_putchar('\n');
+	return (0);
+}

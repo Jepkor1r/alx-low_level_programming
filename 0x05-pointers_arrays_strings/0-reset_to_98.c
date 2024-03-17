99bded30b9350ec7a0106113724f3fc86091b38f
Jepkor1r <jlagat007@gmail.com>
2024-03-17 13:57:47 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - pointer is taken  int as parameter and updates it to 98
+  * @int *n: derefencer
+  *
+  */
+
+void reset_to_98(int *n);
+{
+	int *n = 98;
+}

36eaad4902a89a7301581e7f4e55139e6dd44919
Jepkor1r <jlagat007@gmail.com>
2024-03-19 21:56:03 -0700

    Pointers, arays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _strlen - returns the length of a string
+  * @s: pointer variable
+  *
+  * Return: 0
+  */
+
+int _strlen(char *s)
+{
+	char a;
+	char *s = a;
+	int arr[] = a;
+	int i;
+
+	for (i = arr[0]; i <= sizeof(a) / sizeof(a[0]); i++)
+	{
+		return (i);
+	}
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * _puts - prints a string, followed by a new line, to stdout
+  * @str:  pointer
+  *
+  */
+
+void _puts(char *str)
+{
+	_putchar(*str);
+}
